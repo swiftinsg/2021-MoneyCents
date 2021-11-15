066a2ba5ab2df147d95eac26893b384dde5efc48
PorridgePi <kunbao2006@126.com>
2021-11-15 12:00:31 +0800

    feat: Add HomeView design

--- p1n3appl3/Views/HomeView.swift
+++ p1n3appl3/Views/HomeView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(header: Text("Goals")) {
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Text("Polishing Cloth")
+                            Spacer()
+                            Text("$2.00 / $29.00")
+                        }
+                        .padding(.top)
+                        ProgressView(value: 2, total: 29)
+                            .padding(.bottom)
+                    }
+                }
+                
+                Section(header: Text("Recent Transactions")) {
+                    NavigationLink(destination: Text("Second View")) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Milo")
+                                    .font(.headline)
+                                Text("24 Oct 2021")
+                                    .font(.subheadline)
+                            }
+                            Spacer()
+                            Text("$0.80")
+                                .foregroundColor(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Home")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                }
+            }
+        }
     }
-}
-
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
+    
+    struct HomeView_Previews: PreviewProvider {
+        static var previews: some View {
+            HomeView()
+        }
     }
 }
