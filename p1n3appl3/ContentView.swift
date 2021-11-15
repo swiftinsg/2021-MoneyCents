362822e9c313e4d1f4fe707e63df4e4fb63f8a3e
PorridgePi <kunbao2006@126.com>
2021-11-15 10:15:58 +0800

    Initial Commit

---
+++ p1n3appl3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p1n3appl3
+//
+//  Created by Yikun Zhou on 15/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ p1n3appl3/p1n3appl3App.swift
@@ -0,0 +1,17 @@
+//
+//  p1n3appl3App.swift
+//  p1n3appl3
+//
+//  Created by Yikun Zhou on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct p1n3appl3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
