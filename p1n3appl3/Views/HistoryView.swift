4cbf66be7fc4de55b9d2cc3ab7a119b81af5a4c0
wx <azure.cwx@gmail.com>
2021-11-15 17:37:56 +0800

    Added Navigation title + View

--- p1n3appl3/Views/HistoryView.swift
+++ p1n3appl3/Views/HistoryView.swift
@@ -13,31 +13,30 @@ struct HistoryView: View {
     
     var body: some View {
         
-            // banner
-            VStack(alignment: .center, spacing: 20) {
-                Text("History")
-                    .font(.system(size: 30, weight: .semibold))
-                    .padding(.top,20)
-                
-                // Y-Axis & Segmented picker
-                VStack {
-                    Text("Y-axis")
-                        .font(.system(size: 20, weight: .semibold))
-                        .offset(x: -120, y: 40)
-                    Picker("Y-Axis", selection: $Yaxis) {
-                        Text("Week").tag(0)
-                        Text("Month").tag(1)
-                        Text("Year").tag(2)
-                    }
-                    .pickerStyle(.segmented)
-                    .frame(width: 350, height: 100)
+        
+        NavigationView {
+            
+            
+            // Y-Axis & Segmented picker
+            VStack {
+                Text("Y-axis")
+                    .font(.system(size: 20, weight: .semibold))
+                    .offset(x: -120, y: 130)
+                Picker("Y-Axis", selection: $Yaxis) {
+                    Text("Week").tag(0)
+                    Text("Month").tag(1)
+                    Text("Year").tag(2)
                 }
-                
+                .pickerStyle(.segmented)
+                .frame(width: 350, height: 100)
+                .offset(y: 80)
+                .padding()
+                // ------------------------------------------------------//
                 // X-axis & Segmented picker
                 VStack {
                     Text("X-axis")
                         .font(.system(size: 20, weight: .semibold))
-                        .offset(x: -120)
+                        .offset(x: -120, y: 40)
                     Picker("Xaxis", selection: $Xaxis) {
                         Text("Total").tag(0)
                         Text("").tag(1)
@@ -47,12 +46,18 @@ struct HistoryView: View {
                     .pickerStyle(.segmented)
                     .frame(width: 350, height: 100)
                 }
+                
             }
+            
+            .navigationTitle("History")
+        }
+        
     }
 }
 
+
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
-       HistoryView()
+        HistoryView()
     }
 }
