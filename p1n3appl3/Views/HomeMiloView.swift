754726979b081568d80d3d62772a77e95ec05f1b
yesiluv-alt <yesidow33d@gmail.com>
2021-11-17 16:09:58 +0800

    fix: small edits to miloView

--- p1n3appl3/Views/HomeMiloView.swift
+++ p1n3appl3/Views/HomeMiloView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct HomeMiloView: View {
+    
+    @State var isEditLogPresented = false
+    
     var body: some View {
         NavigationView{
             List{
-                Section{
-                  
-                }
+            
+            
                 Section(header: Text("Information")) {
-                    Text("MIlo")
-                    Text("Date")
-                    Text("Amount")
-                    Text("Category")
+                    Text("24 Octorber 2021")
+                    Text("$0.80")
+                    Text("Food")
+                    Text("bought at school canteen during break")
                     //WIP
                 }
             }.navigationTitle("Milo") //WIP
