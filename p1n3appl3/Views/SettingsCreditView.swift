8b50eee50dcbb29165e604427cd98383480f86c5
yesiluv-alt <yesidow33d@gmail.com>
2021-11-17 15:05:55 +0800

    fix: added brackets to code

--- p1n3appl3/Views/SettingsCreditView.swift
+++ p1n3appl3/Views/SettingsCreditView.swift
@@ -10,58 +10,59 @@ import SwiftUI
 struct SettingsCreditView: View {
     var body: some View {
         NavigationView{
-        List{
-            Section(header:Text("The team")){
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Wei Xuan")
+            List{
+                Section(header:Text("The team")){
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Wei Xuan")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Yi Kun")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Dominic")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Junrui")
+                    }
                 }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Yi Kun")
-                }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Dominic")
-                }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Junrui")
+                Section(header:Text("Special thanks")){
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Jia Chen")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Granwyn")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("Rochelle")
+                    }
+                    HStack{
+                        Image(systemName: "person")
+                            .foregroundColor(.gray)
+                        Text("YJ")
+                    }
+                    .navigationTitle("Credits")
                 }
             }
-            Section(header:Text("Special thanks")){
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Jia Chen")
-            }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Granwyn")
         }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("Rochelle")
-        }
-                HStack{
-                    Image(systemName: "person")
-                        .foregroundColor(.gray)
-                    Text("YJ")
-        }
-        .navigationTitle("Credits")
-    }
-        
-}
-}
-
-struct SettingsCreditView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingsCreditView()
     }
 }
+    
+    struct SettingsCreditView_Previews: PreviewProvider {
+        static var previews: some View {
+            SettingsCreditView()
+        }
+    }
