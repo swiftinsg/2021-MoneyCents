2000dcbec14f1c4b2224aed219cd5b2c2099bf11
demonic8967 <dominic.nyh@gmail.com>
2021-11-17 10:02:34 +0800

    added new goals page

--- p1n3appl3/Views/ContentView.swift
+++ p1n3appl3/Views/ContentView.swift
@@ -42,7 +42,7 @@ struct ContentView: View {
                 }
             
             // Goals
-            GoalsView()
+            GoalsView(goals: .constant([]))
                 .tabItem {
                     Label("Goals", systemImage: "list.dash")
                 }
---
+++ p1n3appl3/Views/NewGoalsView.swift
@@ -0,0 +1,48 @@
+//
+//  NewGoalsView.swift
+//  p1n3appl3
+//
+//  Created by sap on 11/17/21.
+//
+
+import SwiftUI
+
+struct NewGoalsView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var goals: [Goal]
+    @State var goal = Goal(Name: "",
+                         Amount: "")
+
+    var body: some View {
+        NavigationView{
+        Form {
+            Section() {
+                TextField("Name", text: $goal.Name)
+                    .disableAutocorrection(true)
+                TextField("Amount", text: $goal.Amount)
+                    .keyboardType(.numberPad)
+                
+            }
+            
+            Section {
+                Button("Save") {
+                    goals.append(goal)
+                    presentationMode.wrappedValue.dismiss()
+                }
+                Button("Discard") {
+                    presentationMode.wrappedValue.dismiss()
+                }
+                .foregroundColor(.red)
+            }
+        }
+        .navigationTitle("New Goal")
+    }
+}
+}
+
+struct NewGoalsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewGoalsView(goals: .constant([]))
+    }
+}
