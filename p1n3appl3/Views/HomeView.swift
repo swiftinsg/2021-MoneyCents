9ccbd5e8d9e88119b8087bc60e3e2f05ab60d532
wx <Azure.cwx@gmail.com>
2021-11-18 19:29:07 +0800

    Ft: Fixed plus button

--- p1n3appl3/Views/HomeView.swift
+++ p1n3appl3/Views/HomeView.swift
@@ -33,26 +33,28 @@ struct HomeView: View {
                 }
                 
                 Section(header: Text("Recent Transactions")) {
-                    //                    ForEach(logs) { log in
-                    //                        let logIndex = logs.firstIndex(of: log)! // get the index of the current log from logs
-                    //
-                    //                        NavigationLink(destination: LogDetailView(log: $logs[logIndex])) {
-                    //
-                    //                            VStack(alignment: .leading) {
-                    //                                Text(log.name)
-                    //                                    .font(.headline)
-                    //                                Text(log.dateSelector, style: .date)
-                    //                                    .font(.subheadline)
-                    //                            }
-                    //                            Spacer()
-                    //                            Text("$\(log.amount)")
-                    //                                .foregroundColor(.red)
-                    //                        }
-                    
+                    ForEach(logs) { log in
+                        let logIndex = logs.firstIndex(of: log)! // get the index of the current log from logs
+                        
+                        NavigationLink(destination: LogDetailView(log: $logs[logIndex])) {
+                            
+                            VStack(alignment: .leading) {
+                                Text(log.name)
+                                    .font(.headline)
+                                Text(log.dateSelector, style: .date)
+                                    .font(.subheadline)
+                            }
+                            Spacer()
+                            Text("$\(log.amount)")
+                                .foregroundColor(.red)
+                        }
+                    }
                 }
                 .sheet(isPresented: $isNewLogPresented) {
                     NewLogView(logs: $logs)
+                    
                 }
+                
             }
             .listStyle(InsetGroupedListStyle()) // for iOS 15 list style on iOS 14
             .navigationTitle("Home")
@@ -64,7 +66,6 @@ struct HomeView: View {
         }
     }
 }
-//}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
