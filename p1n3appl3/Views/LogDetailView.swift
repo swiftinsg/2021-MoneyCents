f19ac25bee7fdb3e25a161a788f599736cd820a6
PorridgePi <kunbao2006@126.com>
2021-11-18 15:48:04 +0800

    chore: Format code

--- p1n3appl3/Views/LogDetailView.swift
+++ p1n3appl3/Views/LogDetailView.swift
@@ -13,53 +13,46 @@ struct LogDetailView: View {
     @Binding var log: Log
     @State private var text = ""
     
-    
     var body: some View {
-        
-        List{
+        List {
             Section(header: Text("Information")) {
-                Text("24 Octorber 2021")
+                Text("24 October 2021")
                 Text("$0.80")
                 Text("Food")
                 Text("bought at school canteen during break")
                 //WIP
             }
-        }.navigationTitle("Milo") //WIP
-        
-            .navigationBarItems(trailing: Button(action: {
-                isEditPresented.toggle()
-            }, label: {
-                Text("Edit")
-            }))
-            .sheet(isPresented: $isEditPresented) {
-                //insert present code here
-                NavigationView{
-                    List{
-                        Section{
-                         
-                     
-                            TextField("Milo",text: $text)
-                                TextField("Name", text: $text)
-                                TextField("Amount", text: $text)
-                                TextField("Description", text: $text)
-                            
-                        }
+        }
+        .navigationTitle("Milo") //WIP
+        .navigationBarItems(trailing: Button(action: {
+            isEditPresented.toggle()
+        }, label: {
+            Text("Edit")
+        }))
+        .sheet(isPresented: $isEditPresented) {
+            //insert present code here
+            NavigationView {
+                List {
+                    Section {
+                        TextField("Milo",text: $text)
+                        TextField("Name", text: $text)
+                        TextField("Amount", text: $text)
+                        TextField("Description", text: $text)
+                        
                     }
-                   
-                    
-                    
-                        .navigationBarItems(
-                            leading:
-                                Button("Cancel") {
-                                    isEditPresented = false
-                                }.foregroundColor(.red),
-                            trailing:
-                                Button("Save"){
-                                    isEditPresented = false
-                                }
-                        )
                 }
+                .navigationBarItems(
+                    leading:
+                        Button("Cancel") {
+                            isEditPresented = false
+                        }.foregroundColor(.red),
+                    trailing:
+                        Button("Save"){
+                            isEditPresented = false
+                        }
+                )
             }
+        }
     }
 }
 struct LogDetailView_Previews: PreviewProvider {
