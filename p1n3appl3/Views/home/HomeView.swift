3a02d2d5ed427bc4b8c055ddae484ff612a428bb
PorridgePi <kunbao2006@126.com>
2021-11-22 20:41:09 +0800

    feat: Add extension to Color for accentColor to work on iOS 13 and add separate Extensions.swift file

--- p1n3appl3/Views/home/HomeView.swift
+++ p1n3appl3/Views/home/HomeView.swift
@@ -7,14 +7,6 @@
 
 import SwiftUI
 
-extension Date {
-   func getFormattedDate(format: String) -> String {
-        let dateformat = DateFormatter()
-        dateformat.dateFormat = format
-        return dateformat.string(from: self)
-    }
-}
-
 struct HomeView: View {
     
     @State var isNewLogPresented = false
