c53121afe3b7bbe31620f33809fa32594d101531
yesiluv-alt <yesidow33d@gmail.com>
2021-11-22 13:54:15 +0800

    fix: rip bye goalsview

--- p1n3appl3/Views/home/HomeView.swift
+++ p1n3appl3/Views/home/HomeView.swift
@@ -15,7 +15,7 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             List {
-                Section {
+               /* Section {
                     VStack (alignment: .leading) {
                         HStack {
                             Text("Polishing Cloth")
@@ -31,7 +31,7 @@ struct HomeView: View {
                             .accentColor(.white)
                     }
                     .listRowBackground(Color(UIColor(named: "AccentColor") ?? .blue))
-                }
+                } */
                 
                 Section(header: Text("Recent Transactions")) {
                     ForEach(logs.sorted(by: { $0.dateSelector.compare($1.dateSelector) == .orderedDescending })) { log in
