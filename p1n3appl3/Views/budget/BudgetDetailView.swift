25a81634902801c2ef37d6f65e355ba76928c9c3
PorridgePi <kunbao2006@126.com>
2021-11-23 14:48:12 +0800

    feat: Display logs in different categories in BudgetDetailView

--- p1n3appl3/Views/budget/BudgetDetailView.swift
+++ p1n3appl3/Views/budget/BudgetDetailView.swift
@@ -14,7 +14,37 @@ struct BudgetDetailView: View {
     var body: some View {
         List {
             Section(header: Text("Recent Transcations")) {
-                
+                let filteredlogs = logs.filter({$0.category == budget.name})
+                let sortedLogs = filteredlogs.sorted(by: { $0.dateSelector.compare($1.dateSelector) == .orderedDescending })
+                ForEach(sortedLogs) { log in
+                    let logIndex = logs.firstIndex(of: log)! // get the index of the current log from logs
+                    
+                    NavigationLink(destination: LogDetailView(log: $logs[logIndex])) {
+                        Image(systemName: log.icon)
+                            .font(Font.system(size: 16))
+                            .foregroundColor(Color(UIColor(named: "AccentColor") ?? .blue))
+                            .frame(minWidth: 30)
+                        
+                        VStack(alignment: .leading) {
+                            Text(log.name)
+                                .font(.headline)
+                            Text(log.dateSelector.getFormattedDate(format: "d MMM yyyy"))
+                                .font(.subheadline)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(String(format: "%.2f", log.amount))
+                            .foregroundColor(.red)
+                    }
+                }
+                .onDelete { offsets in
+                    for i in offsets {
+                        if let found = logs.firstIndex(where: { $0 == sortedLogs[i] }) {
+                            logs.remove(at: found)
+                        }
+                    }
+                }
             }
         }
         .listStyle(InsetGroupedListStyle())
